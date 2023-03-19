ef390ab4e1104901a1364110ae339eb07c805d04
Oscar Kruithof <ozziekruithof@gmail.com>
2023-03-19 21:07:09 +0100

    Working example for ELF

--- src/ft_nm.h
+++ src/ft_nm.h
@@ -18,6 +18,76 @@
 
 #define LOAD_STRUCTURE(type) *(type *)read_bytes(sizeof(type));
 
+#define PRINT_MACRO(type, target) printf("%30s: ", #target); type(target);
+#define PRINT_MACRO_S(type, target, len) printf("%30s: ", #target); type(target, len);
+
+#define ELF64_ST_BIND(i)   ((i)>>4)
+#define ELF64_ST_TYPE(i)   ((i)&0xf)
+#define ELF64_ST_INFO(b,t) (((b)<<4)+((t)&0xf))
+
+typedef struct s_elf_header
+{
+    u_int32_t   magic_number;
+    u_int8_t    format_bits;
+    u_int8_t    endianness;
+    u_int8_t    elf_version;
+    u_int8_t    target_os;
+    u_int8_t    abi_version;
+    u_int8_t    padding[7];
+    u_int16_t   file_type;
+    u_int16_t   architecture;
+    u_int32_t   elf_version2;    
+} t_elf_header;
+
+typedef struct s_elf_header_extended_64
+{
+    u_int64_t   entry_point_program;
+    u_int64_t   entry_point_pheader;
+    u_int64_t   entry_point_sheader;
+} t_elf_header_extended_64;
+
+typedef struct s_elf_header_extended_32
+{
+    u_int32_t   entry_point_program;
+    u_int32_t   entry_point_pheader;
+    u_int32_t   entry_point_sheader;
+} t_elf_header_extended_32;
+
+typedef struct s_elf_header_final
+{
+    u_int32_t   flags;
+    u_int16_t   header_size;
+    u_int16_t   pheader_size;
+    u_int16_t   pheader_len;
+    u_int16_t   sheader_size;
+    u_int16_t   sheader_len;
+    u_int16_t   sheader_name_index;    
+} t_elf_header_final;
+
+typedef struct s_elf_sheader_64
+{
+    u_int32_t   sheader_name;
+    u_int32_t   section_type;
+    u_int64_t   flags;
+    u_int64_t   file_addr;
+    u_int64_t   file_offset;
+    u_int64_t   section_size;
+    u_int32_t   section_link_idx;
+    u_int32_t   section_info;
+    u_int64_t   section_alignment;
+    u_int64_t   section_total_size;
+} t_elf_sheader_64;
+
+typedef struct s_elf_symbol_64 {
+	u_int32_t	name;
+	u_int8_t	info;
+	u_int8_t	other;
+	u_int16_t	shndx;
+	u_int64_t	value;
+	u_int64_t	size;
+} t_elf_symbol_64;
+
+
 typedef struct s_mach_o_header
 {
     u_int32_t	magic_number;
@@ -85,5 +155,11 @@ void move_to_offset(size_t offset);
 void set_base_offset(size_t offset);
 size_t get_offset();
 
+void print_char(u_int8_t target);
+void print_short(short t);
+void print_int(int t);
+void print_long(long t);
+void print_string(u_int8_t *s, size_t len);
+void print_address(long addr);
 
 #endif
